918c5d9fee155e45f4b184d62d54e7c04b5744db
Martin Owen <martinowenuk@gmail.com>
2009-04-01 23:59:45 +0100

    Added ido settings to custom.el so that filenames that aren't present in the current directory will be created as new files (without prompting) rather than being searched for in other folders.

--- martin/custom.el
+++ martin/custom.el
@@ -4,5 +4,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(ibuffer-saved-filters (quote (("gnus" ((or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode)))) ("programming" ((or (mode . emacs-lisp-mode) (mode . cperl-mode) (mode . c-mode) (mode . java-mode) (mode . idl-mode) (mode . lisp-mode)))))))
+ '(ido-auto-merge-work-directories-length -1)
+ '(ido-create-new-buffer (quote always))
  '(ido-everywhere nil)
  '(ido-mode (quote both) nil (ido)))
