49dd85d09508c3be8022fbf6e308c1645d6ad3b5
Martin Owen <martinowenuk@gmail.com>
2009-04-13 11:47:37 +0100

    Started to use NXHTML mode, replacing PHP mode.

--- martin/lang/php.el
+++ martin/lang/php.el
@@ -2,11 +2,13 @@
 ;; PHP Mode
 ;; ----------------------------------------
 
-(require 'php-mode)
+;;(require 'php-mode)
+
+;; TODO - Should change these to use nxhtml next time I do some Drupal work.
 
 ;; Drupal filename extensions
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
