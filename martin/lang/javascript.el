07f33c6276ec6a73aa5ceb98a82a30afddf8b8e1
Martin Owen <martinowenuk@gmail.com>
2009-04-16 21:06:09 +0100

    Added a couple of helper functions for NXML and JS2 modes.

--- martin/lang/javascript.el
+++ martin/lang/javascript.el
@@ -2,6 +2,13 @@
 ;; Steve Yegge's JS2 Mode for JavaScript
 ;; ----------------------------------------
 
+(defun mo-js2-comment-function ()
+  "Comment the current function."
+  (interactive)
+  (save-excursion
+    (js2-mark-defun)
+    (comment-region (region-beginning) (region-end))))
+
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook 
--- martin/lang/markup.el
+++ martin/lang/markup.el
@@ -3,16 +3,31 @@
 
 (load (concat mo-vendor-dir "/nxhtml/autostart.el"))
 
-;; ----------------------------------------
-;; XML Mode
-;; ----------------------------------------
 
 ;; (load (concat site-lisp "nxml-mode-20041004/rng-auto.el"))
 ;; (add-to-list 'auto-mode-alist 
 ;; 	     '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\|config\\|vbproj\\)\\'" . nxml-mode))
-;; (add-hook 'nxml-mode-hook 
-;; 	  '(lambda () 
-;; 	     (setq nxml-slash-auto-complete-flag t)
-;; 	     (define-key nxml-mode-map (kbd "C-c C-r") 'fo-remove-xml-attribute-region)))
 
-;; (load (concat site-lisp "nxml/autostart.el"))
+;; ----------------------------------------
+;; XML Mode
+;; ----------------------------------------
+
+(defun mo-nxml-mark-node ()
+  "Mark the node contained on the current line."
+  (interactive)
+  (beginning-of-line)
+  (setq beg (point))
+  (nxml-forward-element)
+  (setq end (point))
+  (push-mark beg)
+  (goto-char end)
+  (exchange-point-and-mark))
+
+;; (add-to-list 'auto-mode-alist 
+;; 	     '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\|config\\|vbproj\\)\\'" . nxml-mode))
+(add-hook 'nxml-mode-hook 
+ 	  '(lambda () 
+ 	     (setq nxml-slash-auto-complete-flag t)
+	     (define-key nxml-mode-map (kbd "M-h") 'mo-nxml-mark-node)
+	     (if (eq mo-location 'work)
+		 (define-key nxml-mode-map (kbd "C-c C-r") 'fo-remove-xml-attribute-region))))
