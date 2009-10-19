bb3ebd76b1652eb9bbed7a0db912a7bc3044e106
Martin Owen <martinowenuk@gmail.com>
2009-10-19 18:52:16 +0100

    Turned on auto-revert-mode, and added Textile minor mode for Textile Text buffers.

--- martin/global.el
+++ martin/global.el
@@ -16,6 +16,7 @@
 (savehist-mode 1)		;; Save Minibuffer history
 (show-paren-mode t)		;; Show matching parentheses.
 (global-hl-line-mode 1)         ;; Highlight the current line
+(global-auto-revert-mode 1)     ;; Auto-revert buffers if their files have changed
 (fset 'yes-or-no-p 'y-or-n-p)	;; Accept "y" and "n" for confirmations.
 (put 'upcase-region 'disabled nil)   ;; Don't disable Upper Case Region command
 (put 'downcase-region 'disabled nil) ;; Or Down Case Region
@@ -30,6 +31,8 @@
 ;; Bookmarks
 
 ;; When opening a bookmark, name the buffer after the bookmark
+;; TODO: maybe use uniquify to generate the buffer name? bookmark
+;; name is probably easier to remember
 (add-hook 'bookmark-after-jump-hook
 	  '(lambda ()
 	     (rename-buffer bookmark-current-bookmark)))
--- martin/text.el
+++ martin/text.el
@@ -5,4 +5,9 @@
 (add-hook 'text-mode-hook 
 	  '(lambda ()
 	     (abbrev-mode)
-	     (turn-on-auto-fill)))
+	     (turn-on-auto-fill)
+	     (if (string-match "\\.textile$" (buffer-file-name))
+		 (progn
+		   ;; Turn on Textile Minor Mode for Textile buffers
+		   (require 'textile-minor-mode)
+		   (textile-minor-mode)))))
