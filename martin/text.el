ffe4c172330d2ccf67c7dd820c3b171ff287a8f2
Martin Owen <martinowenuk@gmail.com>
2009-11-04 19:33:28 +0000

    Fix to check for Textile mode.

--- martin/text.el
+++ martin/text.el
@@ -6,7 +6,8 @@
 	  '(lambda ()
 	     (abbrev-mode)
 	     (turn-on-auto-fill)
-	     (if (string-match "\\.textile$" (buffer-file-name))
+	     (if (and (buffer-file-name)
+		      (string-match "\\.textile$" (buffer-file-name)))
 		 (progn
 		   ;; Turn on Textile Minor Mode for Textile buffers
 		   (require 'textile-minor-mode)
