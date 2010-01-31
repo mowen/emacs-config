721efaf3dd35f1c75be1c5cee2f66559a56530c9
Martin Owen <martinowenuk@gmail.com>
2010-01-31 20:53:20 +0000

    No longer use carbon-font, as I'm using standard Emacs on OS X now. Disable truncate-lines in Textile mode.

--- martin/darwin.el
+++ martin/darwin.el
@@ -1,6 +1,1 @@
-;; Set default face in all frames.
-(require 'carbon-font)
-(fixed-width-set-default-fontset
- "-apple-inconsolata-medium-r-normal--14-*-*-*-*-*-iso10646-1")
-
 (setq org-directory (expand-file-name "~/Documents/OrgMode/"))
--- martin/text.el
+++ martin/text.el
@@ -10,4 +10,5 @@
 		 (progn
 		   ;; Turn on Textile Minor Mode for Textile buffers
 		   (require 'textile-minor-mode)
-		   (textile-minor-mode)))))
+		   (textile-minor-mode t)
+		   (setq truncate-lines nil)))))
