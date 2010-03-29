ce6de4904286ffe53790597359bff22bf4d502e7
Martin Owen <martinowenuk@gmail.com>
2010-03-29 19:48:37 +0100

    Use ELPA for package managment.

--- init.el
+++ init.el
@@ -34,3 +34,12 @@
 
 ;; Load location specific file
 (load (concat "martin/" (symbol-name mo-location)))
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
