0fb7e82f16261705efbc265063fef30d2ffa407f
Martin Owen <martinowenuk@gmail.com>
2010-06-13 17:50:03 +0100

    No longer setting the size of the font in OS X, because the default is better.

--- martin/darwin.el
+++ martin/darwin.el
@@ -1,3 +1,2 @@
 (setq org-directory (expand-file-name "~/Documents/OrgMode/"))
-(set-face-attribute 'default nil :height 160 :width 'normal :family "Inconsolata")
 (global-set-key (kbd "M-3") (lambda () (interactive) (insert "#")))
