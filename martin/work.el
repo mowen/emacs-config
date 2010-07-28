8a94b2a4003287b0b7c1bc61017094dab920af4b
Martin Owen <martin.owen@qire.co.uk>
2010-07-28 12:50:18 +0100

    Removed references to Front Office code as I don't use it any more. Also removed mo-work-site-lisp, as I'm going to store everything in vendor.

--- martin/work.el
+++ martin/work.el
@@ -1,18 +1,4 @@
-(setq mo-work-site-lisp "C:/Program Files/emacs-23.1/site-lisp")
-(add-to-list 'load-path mo-work-site-lisp)
-
 (load "martin/work/skeletons")
 (load "martin/work/util")
-(load "martin/work/front-office/front-office-mode")
-(load "martin/work/front-office/front-office-util")
-(load "martin/work/front-office/front-office-email-mode")
 
 (global-hl-line-mode -1) ;; Don't highlight the current line
-
-(add-hook 'nxml-mode-hook
-	  '(lambda ()
-	     ;; Load Front Office Email Minor Mode if the file is in EmailTemplates.
-	     (if (string-match "App_Data/EmailTemplates" (buffer-file-name))
-		 (fo3-email-mode))))
-
-(add-to-list 'auto-mode-alist '("mail.*\\.txt" . front-office-mode))
