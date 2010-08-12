1da11c406418cd4a09dd4259c4c99480bb5c257c
Martin Owen <martin.owen@qire.co.uk>
2010-08-12 09:35:00 +0100

    Using espresso.el for JavaScript rather than js2.el

--- martin/lang/javascript.el
+++ martin/lang/javascript.el
@@ -1,13 +1,6 @@
 ;; ----------------------------------------
-;; Steve Yegge's JS2 Mode for JavaScript
+;; Espresso Mode for JavaScript
 ;; ----------------------------------------
-
-(defun mo-js2-comment-function ()
-  "Comment the current function."
-  (interactive)
-  (save-excursion
-    (js2-mark-defun)
-    (comment-region (region-beginning) (region-end))))
 
 (setq mo-js-imenu-generic-expression
       '((nil "function\\s-+\\(\\w+\\)\\s-*(" 1) ;; Named Function
@@ -15,12 +8,14 @@
         (nil "this\.\\(\\w+\\)\\s-*=\\s-*function\\s-*(" 1) ;; Instance Method
 	(nil "var \\([A-Z]+\\w+\\) = {" 1))) ;; Variable as Class
  
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook 
+(autoload #'espresso-mode "espresso" "Start espresso-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
+
+(add-hook 'espresso-mode-hook
 	  '(lambda ()
-	     (setq js2-bounce-indent-flag nil) ;; Don't do the stupid thing where it cycles through indentations.
-	     (setq imenu-create-index-function 'imenu-default-create-index-function) ;; Don't use the js2 imenu index function
+	     (setq imenu-create-index-function 'imenu-default-create-index-function)
 	     (setq imenu-generic-expression mo-js-imenu-generic-expression)
 	     (capitalized-words-mode t)
 	     (linum-mode t)))
+
