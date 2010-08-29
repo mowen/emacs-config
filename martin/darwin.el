32aff68476cacb8230cfa7dd7fb9500c98e73044
Martin Owen <martinowenuk@gmail.com>
2010-08-29 14:20:30 +0100

    Maximize frame size on startup.

--- martin/darwin.el
+++ martin/darwin.el
@@ -1,2 +1,12 @@
 (setq org-directory (expand-file-name "~/Documents/OrgMode/"))
-(global-set-key (kbd "M-3") (lambda () (interactive) (insert "#")))
+
+;; Bind Alt-3 to #
+(global-set-key (kbd "M-3")
+		(lambda ()
+		  (interactive)
+		  (insert "#")))
+
+;; Maximise the frame (settings are for my MacBook)
+(setq initial-frame-alist
+      `((left . 0) (top . 0)
+        (width . 180) (height . 50)))
