66c368517f72d538bcea8fb97e342fd4deb6cd71
Martin Owen <martin.owen@qire.co.uk>
2011-06-09 09:18:20 +0100

    Enable visual-line-mode and added some ibuffer filters.

--- martin/text.el
+++ martin/text.el
@@ -4,11 +4,12 @@
 (setq default-major-mode 'text-mode)
 (add-hook 'text-mode-hook 
 	  '(lambda ()
-	     (abbrev-mode)
-	     (if (and (buffer-file-name)
-		      (string-match "\\.textile$" (buffer-file-name)))
-		 (progn
-		   ;; Turn on Textile Minor Mode for Textile buffers
-		   (require 'textile-minor-mode)
-		   (textile-minor-mode t)
-		   (setq truncate-lines nil)))))
+	    (abbrev-mode)
+	    (visual-line-mode)
+	    (if (and (buffer-file-name)
+		     (string-match "\\.textile$" (buffer-file-name)))
+		(progn
+		  ;; Turn on Textile Minor Mode for Textile buffers
+		  (require 'textile-minor-mode)
+		  (textile-minor-mode t)
+		  (setq truncate-lines nil)))))
