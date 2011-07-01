433e3adb015f2974b9fd658a74026eab79361856
Martin Owen <martinowenuk@gmail.com>
2011-07-01 22:11:34 +0100

    Show ERC notices in gray.

--- martin/custom.el
+++ martin/custom.el
@@ -11,3 +11,9 @@
    `("schemas.xml"
      ,(concat mo-vendor-dir "/nxml-mode-20041004/schema/schemas.xml")
      ,(concat mo-vendor-dir "/nxml-mode-20041004/schema/html5/html5-schemas.xml"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(erc-notice-face ((((class color) (min-colors 88)) (:foreground "gray" :weight bold)))))
