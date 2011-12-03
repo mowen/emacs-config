fa8edaf1ee1114a083eaa296eb45935839cec89b
Martin Owen <martinowenuk@gmail.com>
2011-12-03 20:56:50 +0000

    Load ruby-mode as a package.

--- mo/lang/ruby.el
+++ mo/lang/ruby.el
@@ -1,28 +1,15 @@
 ;; ----------------------------------------
-;; Ruby Mode Customisations
+;; Ruby Mode
 ;; ----------------------------------------
 
-(add-to-list 'load-path (concat mo-vendor-dir "/ruby"))
+(eval-after-load 'ruby-mode
+  '(progn
+    (require 'ruby-block)
+    (require 'ruby-electric)))
 
-;;(require 'ruby-electric)
-(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.\\(rb\\|gemspec\\|rake\\)$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\(Rakefile\\|Sitefile\\|Gemfile\\)" . ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
-
-(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
 (add-hook 'ruby-mode-hook 
-	  '(lambda () 
-	     (inf-ruby-keys)
-	     ;;(ruby-electric-mode t)
-	     (linum-mode t)
-	     ;;(pabbrev-mode t)
-	     (define-key ruby-mode-map "\C-c\C-a" 'ruby-eval-buffer)))
-
-(defun ruby-eval-buffer () (interactive)
-  "Evaluate the buffer with ruby."
-  (shell-command-on-region (point-min) (point-max) "ruby"))
-
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-;;(add-to-list auto-mode-alist '("\\.rb\\'" . rhtml-mode))
+	  '(lambda ()
+            (ruby-block-mode t)
+            (setq ruby-block-highlight-toggle t)
+            (ruby-electric-mode t)
+            (esk-prog-mode-hook)))
