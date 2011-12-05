6485f744ff0470067fedac7e6017afd5cdf29dbe
Martin Owen <martinowenuk@gmail.com>
2011-12-05 22:59:31 +0000

    Cosmetic changes to OS X setup.

--- mo/darwin.el
+++ mo/darwin.el
@@ -1,4 +1,6 @@
-(setq org-directory (expand-file-name "~/Documents/OrgMode/"))
+(push "/usr/local/bin" exec-path)
+
+(setq org-directory (expand-file-name "~/Documents/org-files/"))
 
 ;; Bind Alt-3 to #
 (global-set-key (kbd "M-3")
@@ -6,7 +8,9 @@
 		  (interactive)
 		  (insert "#")))
 
+(set-frame-font "Menlo-12")
+
 ;; Maximise the frame (settings are for my MacBook)
 (setq initial-frame-alist
       `((left . 0) (top . 0)
-        (width . 180) (height . 50)))
+        (width . 180) (height . 53)))
