634ae3d90e712c6284f1db94cd6380815b84e917
Martin Owen <martinowenuk@gmail.com>
2011-12-18 14:53:33 +0000

    Don't make the erc-notice face so grey, as it's hard to read.

--- mo/custom.el
+++ mo/custom.el
@@ -5,15 +5,9 @@
   ;; If there is more than one, they won't work right.
  '(coffee-js-mode (quote js-mode))
  '(coffee-tab-width 2)
- '(eproject-completing-read-function (quote eproject--ido-completing-read))
  '(ido-auto-merge-work-directories-length -1)
  '(ido-create-new-buffer (quote always))
  '(ido-everywhere nil)
  '(ido-mode (quote both) nil (ido))
  '(rng-schema-locating-files (\` ("schemas.xml" (\, (concat mo-vendor-dir "/nxml-mode-20041004/schema/schemas.xml")) (\, (concat mo-vendor-dir "/nxml-mode-20041004/schema/html5/html5-schemas.xml"))))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(erc-notice-face ((((class color) (min-colors 88)) (:foreground "gray" :weight bold)))))
+
