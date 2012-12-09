dc34870bfea83e0a97024cd6f189c833dff10ee0
unknown <Martin@untitled.(none)>
2012-12-09 11:14:55 +0000

    Minor changes to nREPL config.

--- mo/lang/clojure.el
+++ mo/lang/clojure.el
@@ -1,7 +1,11 @@
 (durendal-enable)
-(add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
 
 ;; nrepl
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(add-to-list 'same-window-buffer-names "*nrepl*") 
-(add-hook 'nrepl-interaction-mode 'paredit-mode)
+(eval-after-load 'nrepl
+  '(progn
+    (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+    (setq nrepl-popup-stacktraces nil)
+    (add-to-list 'same-window-buffer-names "*nrepl*")
+    (add-hook 'nrepl-interaction-mode 'paredit-mode)
+    ;;(add-hook 'nrepl-mode-hook 'clojure-mode-font-lock-setup)
+    (add-hook 'nrepl-mode-hook 'subword-mode)))
