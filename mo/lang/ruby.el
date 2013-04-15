d666391c5cbd6c6f36c60e3e8d138b77c8937b26
Martin Owen <martinowenuk@gmail.com>
2013-04-15 22:05:00 +0100

    Still trying to fix indentation in ruby-mode.

--- mo/lang/ruby.el
+++ mo/lang/ruby.el
@@ -2,6 +2,28 @@
 ;; Ruby Mode
 ;; ----------------------------------------
 
+(defun ruby--jump-to-test ()
+  (find-file
+   (replace-regexp-in-string
+    "/lib/" "/test/"
+    (replace-regexp-in-string
+     "/\\([^/]+\\).rb$" "/test_\\1.rb"
+     (buffer-file-name)))))
+
+(defun ruby--jump-to-lib ()
+  (find-file
+   (replace-regexp-in-string
+    "/test/" "/lib/"
+    (replace-regexp-in-string
+     "/test_\\([^/]+\\).rb$" "/\\1.rb"
+     (buffer-file-name)))))
+
+(defun ruby-jump-to-other ()
+  (interactive)
+  (if (string-match-p "/test/" (buffer-file-name))
+      (ruby--jump-to-lib)
+    (ruby--jump-to-test)))
+
 (eval-after-load 'ruby-mode
   '(progn
     (require 'ruby-electric)
@@ -13,6 +35,12 @@
        (local-set-key [f1] 'yari)
        (ruby-block-mode t)
        (setq ruby-block-highlight-toggle t)
+       ;; Fix silly indentation
+       (setq ruby-deep-indent-paren nil)
+       (setq ruby-deep-indent-paren-style nil)
+       (setq tab-width 2)
+       (set-variable (make-variable-buffer-local 'whitespace-tab-width) 2)
+       (esk-paredit-nonlisp)
        (esk-prog-mode-hook)))
 
     (add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings)
@@ -20,8 +48,8 @@
     (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
     (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
-    ;; Prevent indentation from being added after a paren
-    (setq ruby-deep-indent-paren nil)
+    (define-key ruby-mode-map (kbd "C-c t") 'ruby-jump-to-other)
+
     (defadvice ruby-indent-line (after unindent-closing-paren activate)
      (let ((column (current-column))
            indent offset)
