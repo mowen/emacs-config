bf62d86513d7ebd8cbe58257dbc28a894039a1dd
Martin Owen <martinowenuk@gmail.com>
2013-05-18 22:14:28 +0100

    nrepl hooks

--- mo/lang/clojure.el
+++ mo/lang/clojure.el
@@ -3,11 +3,11 @@
 ;; nrepl
 (eval-after-load 'nrepl
   '(progn
-    (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
     (setq nrepl-popup-stacktraces nil)
     (add-to-list 'same-window-buffer-names "*nrepl*")
     (add-hook 'nrepl-interaction-mode 'paredit-mode)
-    ;;(add-hook 'nrepl-mode-hook 'clojure-mode-font-lock-setup)
+    (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+    (add-hook 'nrepl-mode-hook 'paredit-mode)
     (add-hook 'nrepl-mode-hook 'subword-mode)
-
+    (add-hook 'nrepl-mode-hook 'clojure-mode-font-lock-setup)
     (mo-log "loaded nrepl customisations")))
